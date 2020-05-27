bcf20c602af8e31150d0ba96ed4e6fb0d4555962
arvindreddy <arvindreddypodduturi@gmail.com>
2020-05-27 19:47:21 +0530

    updated sketch.js file

--- mouse_track_in_square/sketch.js
+++ mouse_track_in_square/sketch.js
@@ -1,53 +1,154 @@
+// All the paths
+let paths = [];
+// Are we painting?
+let painting = false;
+// How long until the next circle
+let next = 0;
+// Where are we now and where were we?
+let current;
+let previous;
 let bx;
 let by;
 let boxSize = 150;
 xs = 466;
 ys = 66;
-let num = 15;
-let mx = [];
-let my = [];
-//setup
+let avg;
+
 function setup() {
   createCanvas(720, 400);
+  current = createVector(0,0);
+  previous = createVector(0,0);
   bx = width / 2.0;
   by = height / 2.0;
   rectMode(RADIUS);
-  strokeWeight(2);
 
-  noStroke();
-  fill(255, 153);
-  for (let i = 0; i < num; i++) {
-    mx.push(i);
-    my.push(i);
-  }
+
 }
 
 function draw() {
-  background(255,99,71);
-  rect(bx, by, boxSize, boxSize);
+  background(300);
 
-  if (
-      mouseX > bx - boxSize &&
-      mouseX < bx + boxSize &&
-      mouseY > by - boxSize &&
-      mouseY < by + boxSize
-  )
-  {
+  // If it's time for a new point
+  if (millis() > next && painting) {
 
-    // Cycle through the array, using a different entry on each frame.
-    // Using modulo (%) like this is faster than moving all the values over.
-    let which = frameCount % num;
-    mx[which] = mouseX;
-    my[which] = mouseY;
+    // Grab mouse position      
+    current.x = mouseX;
+    current.y = mouseY;
 
-    for (let i = 0; i < num; i++) {
-      // which+1 is the smallest (the oldest in the array)
-      let index = (which + i) % num;
-      ellipse(mx[index], my[index], i, i);
+    // New particle's force is based on mouse movement
+    let force = p5.Vector.sub(current, previous);
+    force.mult(0);
+
+    // Add new particle
+    paths[paths.length - 1].add(current, force);
+
+    // Schedule next circle
+    next = millis() + random(1000);
+
+    // Store mouse values
+    previous.x = current.x;
+    previous.y = current.y;
+  }
+
+  // Draw all paths
+  for( let i = 0; i < paths.length; i++) {
+    paths[i].update();
+    paths[i].display();
+  }
+}
+
+// Start it up
+function mousePressed() {
+  next = 0;
+  painting = true;
+  previous.x = mouseX;
+  previous.y = mouseY;
+
+  paths.push(new Path());
+}
+
+// Stop
+
+
+// A Path is a list of particles
+class Path {
+  constructor() {
+    this.particles = [];
+    this.hue = random(100);
+  }
+
+  add(position, force) {
+    // Add a new particle with a position, force, and hue
+    this.particles.push(new Particle(position, force, this.hue));
+  }
+
+  // Display plath
+  update() {
+    for (let i = 0; i < this.particles.length; i++) {
+      this.particles[i].update();
     }
-    textSize(16);
-    textAlign(CENTER);
-    text("(" + floor(mouseX-211) + ", " + floor(349-mouseY) + ")", xs,ys);
+  }
 
+  // Display plath
+  display() {
+    // Loop through backwards
+    for (let i = this.particles.length - 1; i >= 0; i--) {
+      // If we shold remove it
+      if (this.particles[i].lifespan <= 0) {
+        this.particles.splice(i, 1);
+        // Otherwise, display it
+      } else {
+        this.particles[i].display(this.particles[i+1]);
+      }
+    }
+
+  }
+}
+
+// Particles along the path
+class Particle {
+  constructor(position, force, hue) {
+    this.position = createVector(position.x, position.y);
+    this.velocity = createVector(force.x, force.y);
+    this.drag = 0.95;
+    this.lifespan = 150;
+  }
+
+  update() {
+    // Move it
+    this.position.add(this.velocity);
+    // Slow it down
+    this.velocity.mult(this.drag);
+    // Fade it out
+    this.lifespan--;
+  }
+
+  // Draw particle and connect it with a line
+  // Draw a line to another
+  display(other) {
+    //if it lies inside the square
+
+
+    rect(bx, by, boxSize, boxSize);
+
+    if (
+        mouseX > bx - boxSize &&
+        mouseX < bx + boxSize &&
+        mouseY > by - boxSize &&
+        mouseY < by + boxSize
+    )
+    {
+      textSize(16);
+      textAlign(CENTER);
+      text("(" + floor(mouseX-211) + ", " + floor(349-mouseY)  + ")", xs,ys);
+
+      stroke(0, this.lifespan);
+      fill(0, this.lifespan/2);
+      ellipse(this.position.x,this.position.y, 8, 8);
+      // If we need to draw a line
+      if (other) {
+        line(this.position.x, this.position.y, other.position.x, other.position.y);
+      }
+    }
   }
 }
