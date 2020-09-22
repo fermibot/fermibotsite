ea19b1c6f8dbf3934ac92685382e6d4988de6d10
fermibot <ashwinuoh@gmail.com>
2020-09-22 06:30:46 -0500

    added a div element to MCAT_countdown.html for creating some extra space

--- pages/MCAT/MCAT_countdown.js
+++ pages/MCAT/MCAT_countdown.js
@@ -1,12 +1,12 @@
 const MCAT_data = {
-    "biology": {"overall_pages": 589, "completed_pages": 59, "passes_required": 3, "passes_done": 0},
+    "biology": {"overall_pages": 589, "completed_pages": 80 + 16, "passes_required": 3, "passes_done": 0},
     "biochemistry": {"overall_pages": 224, "completed_pages": 46, "passes_required": 3, "passes_done": 0},
     "CARS": {"overall_pages": 368, "completed_pages": 51, "passes_required": 3, "passes_done": 0},
     "CARS_workbook": {"overall_pages": 268, "completed_pages": 0, "passes_required": 3, "passes_done": 0},
     "general_chemistry": {"overall_pages": 338, "completed_pages": 44, "passes_required": 3, "passes_done": 0},
     "in_class_companion": {"overall_pages": 253, "completed_pages": 0, "passes_required": 3, "passes_done": 0},
     "organic_chemistry": {"overall_pages": 259, "completed_pages": 32, "passes_required": 3, "passes_done": 0},
-    "physics_and_math": {"overall_pages": 576, "completed_pages": 114, "passes_required": 3, "passes_done": 0},
+    "physics_and_math": {"overall_pages": 576, "completed_pages": 345 + 94, "passes_required": 3, "passes_done": 0},
     "psychology_and_sociology": {"overall_pages": 404, "completed_pages": 36, "passes_required": 3, "passes_done": 0},
     "science_workbook": {"overall_pages": 772, "completed_pages": 0, "passes_required": 3, "passes_done": 0}
 }
