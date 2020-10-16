a878d9119ac1e9253387856c47f9ce8326320cbd
fermibot <ashwinuoh@gmail.com>
2020-10-16 05:52:01 -0500

    added working version of pulsar visualization

--- pages/MCAT/MCAT_countdown.js
+++ pages/MCAT/MCAT_countdown.js
@@ -3,10 +3,15 @@ const MCAT_data = {
     "biochemistry": {"overall_pages": 224, "completed_pages": 46, "passes_required": 3, "passes_done": 0},
     "CARS": {"overall_pages": 368, "completed_pages": 60, "passes_required": 3, "passes_done": 0},
     "CARS_workbook": {"overall_pages": 268, "completed_pages": 0, "passes_required": 3, "passes_done": 0},
-    "general_chemistry": {"overall_pages": 338, "completed_pages": 144 + 50, "passes_required": 3, "passes_done": 0},
+    "general_chemistry": {"overall_pages": 338, "completed_pages": 190 + 50, "passes_required": 3, "passes_done": 0},
     "in_class_companion": {"overall_pages": 253, "completed_pages": 0, "passes_required": 3, "passes_done": 0},
     "organic_chemistry": {"overall_pages": 259, "completed_pages": 32, "passes_required": 3, "passes_done": 0},
-    "physics_and_math": {"overall_pages": 576, "completed_pages": 0 + 94, "passes_required": 3, "passes_done": 1},
-    "psychology_and_sociology": {"overall_pages": 404, "completed_pages": 36, "passes_required": 3, "passes_done": 0},
-    "science_workbook": {"overall_pages": 772, "completed_pages": 0, "passes_required": 3, "passes_done": 0}
+    "physics_and_math": {"overall_pages": 576, "completed_pages": 1 + 94, "passes_required": 3, "passes_done": 1},
+    "psychology_and_sociology": {
+        "overall_pages": 404,
+        "completed_pages": 36 + 29,
+        "passes_required": 3,
+        "passes_done": 0
+    },
+    "science_workbook": {"overall_pages": 772, "completed_pages": 0, "passes_required": 2, "passes_done": 0}
 }
