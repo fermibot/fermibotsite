db7dbdf048db40ca8e8171ba724e2e68938f2b31
fermibot <ashwinuoh@gmail.com>
2020-10-18 13:51:32 -0500

    added radial_tidy_tree.html and radial_tidy_tree.js

--- pages/MCAT/MCAT_countdown.js
+++ pages/MCAT/MCAT_countdown.js
@@ -3,7 +3,7 @@ const MCAT_data = {
     "biochemistry": {"overall_pages": 224, "completed_pages": 46, "passes_required": 3, "passes_done": 0},
     "CARS": {"overall_pages": 368, "completed_pages": 60, "passes_required": 3, "passes_done": 0},
     "CARS_workbook": {"overall_pages": 268, "completed_pages": 0, "passes_required": 3, "passes_done": 0},
-    "general_chemistry": {"overall_pages": 338, "completed_pages": 190 + 50, "passes_required": 3, "passes_done": 0},
+    "general_chemistry": {"overall_pages": 338, "completed_pages": 202 + 50, "passes_required": 3, "passes_done": 0},
     "in_class_companion": {"overall_pages": 253, "completed_pages": 0, "passes_required": 3, "passes_done": 0},
     "organic_chemistry": {"overall_pages": 259, "completed_pages": 32, "passes_required": 3, "passes_done": 0},
     "physics_and_math": {"overall_pages": 576, "completed_pages": 1 + 94, "passes_required": 3, "passes_done": 1},
